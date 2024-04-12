eed0008f305751fc68c01058cdda6f57559acef7
Angel <aburgoapaco@gmail.com>
2024-04-12 13:14:04 +0900

    incrementando conador 5 en 5 archivos modificados en button y app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,26 @@ import './App.css'
 import { Button } from './components/Button'
 import { WapperList } from './components/WrapperList'
 import { List } from './components/List'
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)  
-  
+  const buttonText ="Count:";
    return (
     <>
-      {/* <h1>HOLA DESDE VITE</h1>
-      <Button text={buttonText}/> */}
-      {/* <WapperList/> */}
+      <h1>Contador</h1>
+      <Button text={buttonText}/> 
 
-     <WapperList>
+      
+       {/* <WapperList/> */}
+
+     {/* <WapperList>
      <List/>
-     </WapperList>
+     
+     </WapperList> */}
+     
 
    
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
 
 export const Button = props => {
-
-    const[text] = props ; 'text' 
-    
-    const[count,setCount] =useState(0);
+     // const[text] = props ; 
+     const[count,setCount] = useState(0);
     function handlerButton()    {
-        setCount(count+1);
+        setCount(count+5);
        
     }  
+    const resetCounter = () => {
+        setCount(0);
+      };
     
-    return(
-        <button onClick={handlerButton}>
-            {count} {count}
+    return( 
+        <div>
+        <button onClick={handlerButton} style={{ marginRight: "20px",
+                                                    backgroundColor: "green",
+                                                    color: "white",
+                                                    border: "none", 
+                                                    padding: "12px",
+                                                    borderRadius: "10px" 
+                                                }}>  Iniciar 
         </button>
+        <button onClick={resetCounter}style={{   marginRight: "20px",
+                                                 backgroundColor: "red   ",
+                                                 color: "white", 
+                                                 border: "none", 
+                                                 padding: "12px",
+                                                  borderRadius: "10px"
+                                             }}>     Resetear
+         </button>
+         <div style={{paddingTop:"25px",
+                     fontSize:"50px",
+                     textAlign:"center"
+                     }}> {count}</div>
+         </div> 
     );
 } 
